a8bdb4e8d557802285a0d60ebbe0b27a9cd271dd
Dina <dina@Dinas-MBP.local>
2022-01-28 17:09:32 +0300

    init

---
+++ app/src/main/java/net/mrshipper/mrshipper/MainActivity.java
@@ -0,0 +1,27 @@
+package net.mrshipper.mrshipper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        WebView.setWebContentsDebuggingEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+        webView.loadUrl("https://test.mrshipper.net/");
+    }
+}
